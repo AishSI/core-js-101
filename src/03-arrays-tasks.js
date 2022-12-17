2cb098e1073993f240485fa444c94fae62f8543b
AishSI <aishsi@gmail.com>
2022-12-18 03:20:48 +0700

    feat: add 15 solution 03-arrays-task edit linter

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -252,6 +252,7 @@ function toArrayOfSquares(arr){
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
 */
 function getMovingSum(arr) {
+  throw new Error( 'Not implemented' );
   // return arr.reduce((accum, item) => (accum.push(item), accum), [])  
 }
 
