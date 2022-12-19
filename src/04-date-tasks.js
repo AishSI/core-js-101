be154e12b96c922bd82d8d40b6f3d9b3cb25a717
AishSI <aishsi@gmail.com>
2022-12-19 12:46:19 +0700

    feat: add solution 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,23 +19,23 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
  * Parses an ISO 8601 string date representation into date value
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
- *
- * @param {string} value
- * @return {date}
- *
- * @example :
- *    '2016-01-19T16:07:37+00:00'    => Date()
- *    '2016-01-19T08:07:37Z' => Date()
- */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+*
+* @param {string} value
+* @return {date}
+*
+* @example :
+*    '2016-01-19T16:07:37+00:00'    => Date()
+*    '2016-01-19T08:07:37Z' => Date()
+*/
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  return (!(year % 4) && !!(year % 100)) || !(year % 400);
 }
 
 
@@ -73,8 +74,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return new Date(endDate.getTime() - startDate.getTime()).toISOString().slice(11, -1);
 }
 
 
@@ -94,8 +95,17 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  // уже решал:
+  // 6kyu https://www.codewars.com/kata/543ddf69386034670d000c7d/train/javascript
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const gradHours = ((hours % 12) * 30) + (0.5 * minutes); // (1 минута = 0,5 градуса)
+  const gradMinute = (minutes % 60) * 6; // градусы минутной стрелки
+  const angle = Math.abs(gradHours - gradMinute);
+  const minAngle = Math.min(angle, 360 - angle);
+
+  return (minAngle * Math.PI) / 180;
 }
 
 
